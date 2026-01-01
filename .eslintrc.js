6b2c26246ece933d03f969e8d18cd5ddf81f3c7e
Built with Arvad.ai <manmadeconsulting2@gmail.com>
2026-01-01 21:50:57 +0100

    Auto-merge: Setup Project Foundation and Build Configuration
    Automatically merged PR for task 1d2ec56e-a53b-4cd3-9f76-5fd8c24e1830

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'eqeqeq': ['error', 'always'],
+    'curly': ['error', 'all'],
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-new-func': 'error',
+    'no-script-url': 'error',
+    'no-alert': 'warn',
+    'no-debugger': 'warn',
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    '*.config.js',
+  ],
+};
